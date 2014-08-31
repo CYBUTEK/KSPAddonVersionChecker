3bf59e15c76a54ec5b98d512367b18735bff215b
CYBUTEK <admin@cybutek.net>
2014-08-31 03:19:03 +0100

    Refactored: AddonListGui

--- KSP-AVC/AddonListGui.cs
+++ KSP-AVC/AddonListGui.cs
@@ -15,28 +15,23 @@
 //     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // 
 
-#region Using Directives
-
 using System;
 
 using UnityEngine;
-
-#endregion
 
 namespace KSP_AVC
 {
     [KSPAddon(KSPAddon.Startup.Instantly, false)]
     public class AddonListGui : MonoBehaviour
     {
-        #region Fields
-
         private DropDownList dropDownList;
 
-        #endregion
+        private GUIStyle labelStyleLeft;
+        private GUIStyle labelStyleLeftIssue;
+        private GUIStyle labelStyleRight;
+        private GUIStyle labelStyleRightIssue;
 
-        #region Initialisation
-
-        private void Awake()
+        protected void Awake()
         {
             try
             {
@@ -48,124 +43,7 @@ namespace KSP_AVC
             }
         }
 
-        private void Start()
-        {
-            try
-            {
-                this.InitialiseStyles();
-                this.dropDownList = this.gameObject.AddComponent<DropDownList>();
-                this.dropDownList.DrawCallback = this.DrawListItems;
-            }
-            catch (Exception ex)
-            {
-                Logger.Exception(ex);
-            }
-        }
-
-        #endregion
-
-        #region Styles
-
-        private GUIStyle labelStyleLeft;
-        private GUIStyle labelStyleLeftIssue;
-        private GUIStyle labelStyleRight;
-        private GUIStyle labelStyleRightIssue;
-
-        private void InitialiseStyles()
-        {
-            try
-            {
-                this.labelStyleLeft = new GUIStyle
-                {
-                    normal =
-                    {
-                        textColor = Color.white
-                    },
-                    padding = new RectOffset(5, 5, 3, 3),
-                    fontStyle = FontStyle.Bold,
-                    alignment = TextAnchor.MiddleLeft,
-                    stretchWidth = true
-                };
-
-                this.labelStyleLeftIssue = new GUIStyle(this.labelStyleLeft)
-                {
-                    normal =
-                    {
-                        textColor = Color.yellow
-                    }
-                };
-
-                this.labelStyleRight = new GUIStyle(this.labelStyleLeft)
-                {
-                    alignment = TextAnchor.LowerRight
-                };
-
-                this.labelStyleRightIssue = new GUIStyle(this.labelStyleRight)
-                {
-                    normal =
-                    {
-                        textColor = Color.yellow
-                    }
-                };
-            }
-            catch (Exception ex)
-            {
-                Logger.Exception(ex);
-            }
-        }
-
-        #endregion
-
-        #region Updating
-
-        private void Update()
-        {
-            try
-            {
-                if (HighLogic.LoadedScene == GameScenes.SPACECENTER)
-                {
-                    Destroy(this);
-                }
-            }
-            catch (Exception ex)
-            {
-                Logger.Exception(ex);
-            }
-        }
-
-        #endregion
-
-        #region Drawing
-
-        private void OnGUI()
-        {
-            if (HighLogic.LoadedScene == GameScenes.SETTINGS)
-            {
-                return;
-            }
-
-            if (AddonLibrary.Populated)
-            {
-                this.dropDownList.DrawButton("Show All KSP-AVC Ready Add-Ons", new Rect(), 400.0f);
-            }
-        }
-
-        private void DrawListItems(DropDownList list)
-        {
-            foreach (var addon in AddonLibrary.Addons)
-            {
-                GUILayout.BeginHorizontal();
-                GUILayout.Label(addon.Name, addon.IsUpdateAvailable || !addon.IsCompatible ? this.labelStyleLeftIssue : this.labelStyleLeft);
-                GUILayout.Label(addon.LocalInfo.Version.ToString(), addon.IsUpdateAvailable || !addon.IsCompatible ? this.labelStyleRightIssue : this.labelStyleRight);
-                GUILayout.EndHorizontal();
-            }
-        }
-
-        #endregion
-
-        #region Destruction
-
-        private void OnDestroy()
+        protected void OnDestroy()
         {
             try
             {
@@ -180,6 +58,95 @@ namespace KSP_AVC
             }
         }
 
-        #endregion
+        protected void OnGUI()
+        {
+            if (HighLogic.LoadedScene == GameScenes.SETTINGS)
+            {
+                return;
+            }
+
+            this.dropDownList.DrawButton("Show All KSP-AVC Ready Add-Ons", new Rect(), 400.0f);
+        }
+
+        protected void Start()
+        {
+            try
+            {
+                this.InitialiseStyles();
+                this.dropDownList = this.gameObject.AddComponent<DropDownList>();
+                this.dropDownList.DrawCallback = this.DrawListItems;
+            }
+            catch (Exception ex)
+            {
+                Logger.Exception(ex);
+            }
+        }
+
+        protected void Update()
+        {
+            try
+            {
+                if (HighLogic.LoadedScene == GameScenes.SPACECENTER)
+                {
+                    Destroy(this);
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.Exception(ex);
+            }
+        }
+
+        private void DrawListItems(DropDownList list)
+        {
+            if (AddonLibrary.Addons == null)
+            {
+                return;
+            }
+
+            foreach (var addon in AddonLibrary.Addons)
+            {
+                GUILayout.BeginHorizontal();
+                GUILayout.Label(addon.Name, addon.IsUpdateAvailable || !addon.IsCompatible ? this.labelStyleLeftIssue : this.labelStyleLeft);
+                GUILayout.Label(addon.LocalInfo.Version.ToString(), addon.IsUpdateAvailable || !addon.IsCompatible ? this.labelStyleRightIssue : this.labelStyleRight);
+                GUILayout.EndHorizontal();
+            }
+        }
+
+        private void InitialiseStyles()
+        {
+            this.labelStyleLeft = new GUIStyle
+            {
+                normal =
+                {
+                    textColor = Color.white
+                },
+                padding = new RectOffset(5, 5, 3, 3),
+                fontStyle = FontStyle.Bold,
+                alignment = TextAnchor.MiddleLeft,
+                stretchWidth = true
+            };
+
+            this.labelStyleLeftIssue = new GUIStyle(this.labelStyleLeft)
+            {
+                normal =
+                {
+                    textColor = Color.yellow
+                }
+            };
+
+            this.labelStyleRight = new GUIStyle(this.labelStyleLeft)
+            {
+                alignment = TextAnchor.LowerRight
+            };
+
+            this.labelStyleRightIssue = new GUIStyle(this.labelStyleRight)
+            {
+                normal =
+                {
+                    textColor = Color.yellow
+                }
+            };
+        }
     }
 }
