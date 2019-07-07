2ce1864eda6acd34d93ae494f627eb0f5d4a581d
Jonathan Bayer <linuxgurugamer@gmail.com>
2019-07-06 22:15:36 -0400

    Version bump for 1.7.2  Updated AssemblyVersion.tt

--- deploy.bat
+++ deploy.bat
@@ -7,12 +7,12 @@ rem GAMEDATA is the name of the local GameData
 rem VERSIONFILE is the name of the version file, usually the same as GAMEDATA,
 rem    but not always
 
-set H=R:\KSP_1.6.1_dev
+set H=R:\KSP_1.7.2_dev
 set GAMEDIR=KSP-AVC
 set GAMEDATA="GameData"
 set VERSIONFILE=%GAMEDIR%.version
 
-copy /Y "%1%2" "%GAMEDATA%\%GAMEDIR%"
+copy /Y "%1%2" "%GAMEDATA%\%GAMEDIR%\Plugins"
 copy /Y %VERSIONFILE% %GAMEDATA%\%GAMEDIR%
 
 xcopy /y /s /I %GAMEDATA%\%GAMEDIR% "%H%\GameData\%GAMEDIR%"
